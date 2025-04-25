ef67722fdb9a9339cb7c537f985bd295e1f5b16d
Sebas <saabad@pucp.edu.pe>
2025-04-24 20:44:53 -0500

    first commit

---
+++ Lab3_20206464/src/main/java/com/example/lab3_20206464/Lab320206464Application.java
@@ -0,0 +1,14 @@
+package com.example.lab3_20206464;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320206464Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320206464Application.class, args);
+    }
+
+}
---
+++ Lab3_20206464/src/test/java/com/example/lab3_20206464/Lab320206464ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20206464;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320206464ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
